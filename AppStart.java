903292feb67dd0f2ad0bc80f332bb89924dcb852
Tiago Lourenço <201900848@estudantes.ips.pt>
2024-03-13 10:38:27 +0000

    lab0-Final

--- AppStart.java
+++ AppStart.java
@@ -1,29 +1,23 @@
+
+/**
+ * Write a description of class AppStart here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
 public class AppStart {
+
     public static void main(String[] args) {
-        // Teste da classe enumerada Size
+        Size size = Size.MEDIUM;
         
-        // Obtendo e imprimindo as descrições de cada tamanho
-        System.out.println("Descrições dos tamanhos:");
-        System.out.println("Pequeno: " + Size.SMALL.getDescription());
-        System.out.println("Médio: " + Size.MEDIUM.getDescription());
-        System.out.println("Grande: " + Size.LARGE.getDescription());
+        System.out.println("Name         : "+size.name());
+        System.out.println("toString()   : \""+size.toString().trim()+"\"");
+        System.out.println("Ordem        : "+size.ordinal());
+        System.out.println("Valor minimo : "+size.getMinValue());
+        System.out.println("Valor maximo : "+size.getMaxValue());
+        System.out.println("Código       : \'"+size.getCode()+"\'");
         
-        // Obtendo e imprimindo os valores mínimos de cada tamanho
-        System.out.println("\nValores mínimos dos tamanhos:");
-        System.out.println("Pequeno: " + Size.SMALL.getMinValue());
-        System.out.println("Médio: " + Size.MEDIUM.getMinValue());
-        System.out.println("Grande: " + Size.LARGE.getMinValue());
         
-        // Obtendo e imprimindo os valores máximos de cada tamanho
-        System.out.println("\nValores máximos dos tamanhos:");
-        System.out.println("Pequeno: " + Size.SMALL.getMaxValue());
-        System.out.println("Médio: " + Size.MEDIUM.getMaxValue());
-        System.out.println("Grande: " + Size.LARGE.getMaxValue());
-        
-        // Obtendo e imprimindo os códigos de cada tamanho
-        System.out.println("\nCódigos dos tamanhos:");
-        System.out.println("Pequeno: " + Size.SMALL.getCode());
-        System.out.println("Médio: " + Size.MEDIUM.getCode());
-        System.out.println("Grande: " + Size.LARGE.getCode());
     }
+    
 }
